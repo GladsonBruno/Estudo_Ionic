9c82906194fb3373e39f614cbc057c8921fba7b9
Gladson Santos <gladsonbruno16@gmail.com>
2017-12-30 17:01:05 -0200

    Commit inicial

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ActionSheetController, AlertController } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,48 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  title: string = "Index";
 
+  constructor(public navCtrl: NavController, public actionSheetController: ActionSheetController,
+              public alertCtrl: AlertController) {
+
+  }
+
+  mostrarActionSheet(){
+    let actionSheet = this.actionSheetController.create({
+      title: "Album",
+      buttons: [
+        {
+          text: 'Destructive',
+          role: 'destructive',
+          handler: () => {
+            alert("Destructive");
+          }
+        },{
+          text: 'Archive',
+          handler: () => {
+            alert("Archive");
+          }
+        },{
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            alert("Cancel");
+          }
+        }
+      ]
+    });
+
+    actionSheet.present();
+  }
+
+  mostrarAlertaBasico(){
+    let alert = this.alertCtrl.create({
+      title: "Alerta Basico",
+      subTitle: "Teste Alerta Basico",
+      buttons: ['OK']
+    });
+    alert.present();
   }
 
 }
